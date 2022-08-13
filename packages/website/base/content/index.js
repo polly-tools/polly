e625790cf2c1752a0ceb02cabb8bb134357a30d8
troels_a <troels@blck.dk>
2022-08-13 12:26:50 +0200

    copy

--- packages/website/base/content/index.js
+++ packages/website/base/content/index.js
@@ -1,9 +1,9 @@
 export default <>
-Polly is a protocol for creating, deploying and configuring reusable smart contract modules in custom configurations directly on-chain. The framework makes it easier and cheaper to deploy code to the Ethereum blockchain. Polly is (1) OPEN for all to use and build on through an (2) EXTENDABLE architecture. The Polly core, all module versions and configurations are (3) PERMANENT once deployed.
+Polly is a protocol for creating, deploying and configuring reusable smart contract modules in custom configurations directly on-chain. The framework makes it easier and cheaper to deploy code to the Ethereum blockchain. Polly is (1) OPEN for all to use and build on through a highly (2) EXTENDABLE architecture. The Polly core, all module versions and user configurations are (3) PERMANENT once deployed.
 </>
 
 export const open = <>
-The projects and protocols of the future must be accesible and open to all and so must the tools with which they are build. We've built Polly to ensure access for everyone by making the foundational access points fully permissionless.
+The projects and protocols of the future must be accesible and open to all and so must the tools with which they are build. Polly is structured to ensure access for everyone by making the foundational entry points fully permissionless.
 </>
 
 export const extendable = <>
@@ -11,5 +11,5 @@ The modular approach enables mix and match between Polly modules and custom cont
 </>
 
 export const permanent = <>
-The Polly core is a non-upgradeable smart contract that stores references to modules and configruations. Once a module is added to the library it can't be removed or changed and will forever be accesible.
+At the core of Polly is a non-upgradeable smart contract that stores references to modules and configurations. Once a module is added to the library it can't be removed or changed and will forever be accesible in all its versions.
 </>
