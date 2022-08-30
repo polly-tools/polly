6643e1af13425853f63d49f7e1f9611b7706bdac
troels_a <troels@blck.dk>
2022-08-31 00:08:19 +0200

    provider status option

--- packages/core/contracts/modules/TokenRegistry.sol
+++ packages/core/contracts/modules/TokenRegistry.sol
@@ -18,6 +18,7 @@ contract TokenRegistry is PollyModule {
         string _function; // The function signature of the provider
         string _token_url; // A direct url to a token - use {id} to insert the token id
         string _url; // A main url of the provider
+        uint _status; // 0 - enabled, 1 - disabled, 2 - banned
     }
 
     struct Token {
@@ -75,6 +76,15 @@ contract TokenRegistry is PollyModule {
       _providers[provider_._address] = provider_;
     }
 
+
+    /// @dev set provider status
+    /// @param provider_ The provider to set status for
+    /// @param status_ The status to set - 0 - enabled, 1 - disabled, 2 - banned
+    function setProviderStatus(address provider_, uint status_) public onlyManager {
+      require(status_ <= 2, 'INVALID_STATUS');
+      _providers[provider_]._status = status_;
+    }
+
     /// @dev Get the provider information
     /// @param address_ The address of the provider
     /// @return The provider information
@@ -97,6 +107,13 @@ contract TokenRegistry is PollyModule {
     }
 
 
+    /// @dev private function to check provider status
+    /// @param provider_ The provider to check the status of
+    /// @param status_ The status to check against
+    /// @return True if the provider status is equal to the status_ parameter
+    function _reqProviderStatus(address provider_, uint status_) private view returns (bool) {
+      return _providers[provider_]._status <= status_;
+    }
 
 
     /// TOKENS
@@ -111,23 +128,40 @@ contract TokenRegistry is PollyModule {
         return _count;
     }
 
+
     /// @dev Register a new token
     /// @param internal_id_ The id of the token
     /// @return id the id of the newly registered token
     function registerToken(uint internal_id_) public returns(uint) {
+
+      if(_providers[msg.sender]._status != 0)
+        return 0; // provider is disabled or banned
+
       require(msg.sender == _providers[msg.sender]._address, 'ONLY_PROVIDER');
+
       return _registerToken(msg.sender, internal_id_);
+
     }
+
 
     /// @dev batch register tokens for a provider
     /// @param provider_ The provider of the tokens
     /// @param ids_ The ids of the tokens
-    function batchRegisterTokens(address provider_, uint[] memory ids_) public {
+    function batchRegisterTokens(address provider_, uint[] memory ids_) public returns(uint[] memory) {
+
+      if(_providers[msg.sender]._status != 0)
+        return new uint[](0); // provider is disabled or banned
+
       require(_provider_controllers[msg.sender][provider_], 'ONLY_PROVIDER_CONTROLLER');
+      uint[] memory result_ = new uint[](ids_.length);
       for (uint i = 0; i < ids_.length; i++) {
-        _registerToken(provider_, ids_[i]);
+       result_[i] = _registerToken(provider_, ids_[i]);
       }
+
+      return result_;
+
     }
+
 
     /// @dev Get the total token count
     /// @return The total token count
@@ -135,10 +169,15 @@ contract TokenRegistry is PollyModule {
         return _count;
     }
 
+
     /// @dev Get the token information
     /// @param id_ The id of the token
     /// @return The token information
     function getToken(uint id_) public view returns(TokenInformation memory){
+
+        Provider memory provider_ = _providers[_tokens[id_]._provider];
+
+        require(provider_._status <= 2, 'PROVIDER_BANNED');
 
         (bool has_meta_, bytes memory uri_) = _tokens[id_]._provider.staticcall(
           abi.encodeWithSignature(_providers[_tokens[id_]._provider]._function, _tokens[id_]._id)
@@ -151,5 +190,6 @@ contract TokenRegistry is PollyModule {
         );
 
     }
+
 
 }
