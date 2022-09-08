abeff2b8470d24014b88e1d8632f6800ac667272
Troels Abrahamsen <troels@blck.dk>
2022-09-08 18:38:40 +0200

    Add configure module to hardhat-plugin

--- packages/hardhat-polly/index.js
+++ packages/hardhat-polly/index.js
@@ -53,5 +53,47 @@ extendEnvironment(async (hre) => {
   }
 
 
+  hre.polly.configureModule = async (name, options = {}) => {
+
+    const [owner] = await hre.ethers.getSigners();
+
+
+    const {
+      signer,
+      version,
+      params,
+      store,
+      configName,
+      ...o
+    } = Object.assign({
+
+      version: 0, // Latest version
+      params: [], // No params
+      store: true, // Store config in Polly
+      configName: '', // No config name
+      signer: owner
+
+    }, options);
+
+
+    // Configure MetaForIds
+    const tx = await polly.connect(signer).configureModule(
+      name, // Name
+      version, // Latest version
+      params, // No params
+      store, // Store config in Polly
+      configName // No config name
+    );
+
+    const receipt = await tx.wait();
+    const args = receipt.events.filter(x => x.event === "moduleConfigured").map(event => event.args[2]);
+    const index = args[args.length-1];
+    const config = await polly.getConfigsForAddress(signer.address, 1, index, false);
+    module = await ethers.getContractAt(name, config[0].params[0]._address);
+    return module;
+
+  }
+
+
 
 });
