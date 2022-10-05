af30a976625e2343d1a097e3a831936431d2b28e
troels_a <troels@blck.dk>
2022-10-05 22:06:46 +0200

    Auto truncate and copy button for Address component

--- packages/website/components/Address/Address.js
+++ packages/website/components/Address/Address.js
@@ -1,15 +1,34 @@
 import copy from "copy-to-clipboard";
 import styled from "styled-components";
 
-const Copy = styled.span`
+const Copy = styled.a`
+  line-height: 1em;
+  display: inline-block;
+  width: 1.1em;
+  height: 1.1em;
   cursor: pointer;
+  border: 1px solid ${props => props.theme.colors.primary};
+  border-radius: 4px;
+  transition: all 50ms ease-in-out;
   &:active {
     opacity: 0.5;
+    transform: scale(0.9);
+  }
+  &:after {
+    content: "+";
+    display: block;
+    position: relative;
+    top: -0.1em;
+    left: 0.15em;
   }
 `;
 
-export default function Address({address, copyEnabled}){
+function truncate(address){
+  return address.slice(0, 6) + "..." + address.slice(-4);
+}
+
+export default function Address({address}){
   return <span>
-    {address} <Copy onClick={() => copy(address)}>copy</Copy>
+    {truncate(address)} <Copy title="Copy to clipboard" onClick={() => copy(address)}/>
   </span>;
 }
