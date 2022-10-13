42091a67b7e48d2ece7b7b41592f084f8a92c882
troels_a <troels@blck.dk>
2022-10-13 17:57:14 +0200

    regen module abis + scripts

---
+++ scripts/compile-module.js
@@ -0,0 +1,14 @@
+// Test module
+require('dotenv').config();
+const path = require('path');
+const { execSync } = require('child_process');
+
+const name = process.argv[2]
+const safeName = name.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase().replace(/^-/, '');
+const modulePath = path.join(__dirname, '..', `packages/module-${safeName}`);
+
+async function testModule(name) {
+  execSync(`npx hardhat compile`, { cwd: modulePath, stdio: 'inherit', env: process.env});
+}
+
+testModule(name);
