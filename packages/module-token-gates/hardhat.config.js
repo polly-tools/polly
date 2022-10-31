28d75f2c66f04055f5fd4217edbeec0f12a93337
troels_a <troels@blck.dk>
2022-10-31 15:24:38 +0100

    added module-token-gates package

---
+++ packages/module-token-gates/hardhat.config.js
@@ -0,0 +1,27 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-abi-exporter");
+require("hardhat-gas-reporter");
+require("@polly-tools/hardhat-polly");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+  polly: {
+    fork: {
+      localhost: process.env.POLLY_ADDRESS
+    }
+  },
+  gasReporter: {
+    enabled: true,
+    gasPrice: 25,
+    currency: 'ETH',
+    coinmarketcap: process.env.CMC_API_KEY
+  },
+  abiExporter: {
+    runOnCompile: true,
+    path: "./abi",
+    only: ["TokenGates"],
+    clear: true,
+    flat: true,
+  }
+};
